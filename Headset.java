ccc1393249ab5d3f631ff5b6340c66698cb2af18
Darshan8500 <darshanhampali142@gmail.com>
2025-06-17 14:31:31 +0530

    sop

---
+++ Headset.java
@@ -0,0 +1,8 @@
+class Headset
+{
+public static void main(String args[])
+{
+System.out.println(" 60H Long Battery Life – Boult Fluid X wireless over-ear headphones deliver an impressive 60 hours of playtime, making them ideal Bluetooth headphones for travel.");
+}
+
+}
